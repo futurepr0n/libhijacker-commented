ce5a9ddd094b50379ce197e9a1d3dfb06fc451bb
Mark Pereira <me@markpereira.com>
2023-06-19 09:44:19 -0400

    Update backtrace.cpp with comments

--- libhijacker/source/backtrace.cpp
+++ libhijacker/source/backtrace.cpp
@@ -1,52 +1,49 @@
 #include "backtrace.hpp"
 
-// Declaration of the function `getFramePointer()`
+// Function to retrieve the frame pointer of the current function
 const Frame * __attribute__((naked)) getFramePointer() {
-    // Inline assembly code using AT&T syntax
-    __asm__ volatile(
-        "push %rbp\n"   // Push the value of the base pointer onto the stack
-        "pop %rax\n"    // Pop the value from the stack into the return register
-        "ret\n"         // Return from the function
-    );
+	__asm__ volatile(
+		"push %rbp\n"   // Save the value of the rbp register by pushing it onto the stack
+		"pop %rax\n"    // Move the value from the stack to the rax register
+		"ret\n"         // Return from the function
+	);
 }
 
-// Declaration of the external function `puts()`
+// Declaration of the external function 'puts'
 extern "C" int puts(const char*);
 
-// Definition of the function `getTextStart()`
+// Function to get the start address of the text segment (code)
 static uintptr_t __attribute__((naked, noinline)) getTextStart() {
-    asm volatile(
-        "lea __text_start(%rip), %rax\n"    // Load the address of __text_start into the return register
-        "ret\n"                             // Return from the function
-    );
+	asm volatile(
+		"lea __text_start(%rip), %rax\n"   // Load the address of the symbol '__text_start' into rax
+		"ret\n"                            // Return from the function
+	);
 }
 
-// Definition of the function `getTextEnd()`
+// Function to get the end address of the text segment (code)
 static uintptr_t __attribute__((naked, noinline)) getTextEnd() {
-    asm volatile(
-        "lea __text_stop(%rip), %rax\n"     // Load the address of __text_stop into the return register
-        "ret\n"                             // Return from the function
-    );
+	asm volatile(
+		"lea __text_stop(%rip), %rax\n"    // Load the address of the symbol '__text_stop' into rax
+		"ret\n"                            // Return from the function
+	);
 }
 
-// Definition of the function `printBacktrace()`
+// Function to print the backtrace
 void printBacktrace() {
-    const uintptr_t start = getTextStart();   // Get the start address of the text section
-    const uintptr_t stop = getTextEnd();      // Get the end address of the text section
+	const uintptr_t start = getTextStart();   // Get the start address of the text segment
+	const uintptr_t stop = getTextEnd();      // Get the end address of the text segment
+	__builtin_printf(".text: 0x%08llx\n", start);   // Print the start address
 
-    // Print the start address of the text section
-    __builtin_printf(".text: 0x%08llx\n", start);
+	puts("---backtrace start---");
 
-    // Print backtrace header
-    puts("---backtrace start---");
-
-    // Iterate over the frames of the backtrace
+	// Loop through the call frames
 	for (const Frame *__restrict frame = getFramePointer(); frame != nullptr; frame = frame->next) {
-		if (frame->addr != 0) [[likely]] {
-			if (frame->addr >= start && frame->addr <= stop) {
-				__builtin_printf("0x%llx\n", frame->addr - start);
+		if (frame->addr != 0) [[likely]] {   // Check if the frame address is not zero
+			if (frame->addr >= start && frame->addr <= stop) {   // Check if the frame address is within the text segment
+				__builtin_printf("0x%llx\n", frame->addr - start);   // Print the relative address within the text segment
 			}
 		}
 	}
+
 	puts("---backtrace end---");
 }
